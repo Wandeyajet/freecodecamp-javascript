8cd06ee5b624756e39a1005fc26124b600cd1b7a
Wandeyajet <a.yewande01@gmail.com>
2023-11-01 13:34:38 +0100

    adjustmt

---
+++ script.js
@@ -0,0 +1,7 @@
+let a = 39;
+console.log(a += 10);
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+ fact = "is awesome!";
+console.log(FCC, fact); // Change this line
